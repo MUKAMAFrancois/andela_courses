b6ae9cd4a83a4e13e802c454626969e2b0e442a8
Mukama <mk.frmh3@gmail.com>
2024-01-14 23:18:12 +0200

    first commit

---
+++ courses/htmlCss/front_End/2023-CHJ/JS/todo1.js
@@ -0,0 +1,16 @@
+
+let task1='Wake Up';
+let task2="Washing";
+let task3="take Breakfast";
+let element= document.createElement('div');// create an html element
+element.innerText=task1;
+document.body.appendChild(element);// display
+//display second task
+element=document.createElement('div');
+element.innerText=task2;
+document.body.appendChild(element);
+//dispaly third task
+element=document.createElement('div');
+element.innerText=task3;
+document.body.appendChild(element);
+
